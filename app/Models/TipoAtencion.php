15451bedd88b96620478cc73aacf59fe7198b86c
franklinantonio08 <franklinantonio08@gmail.com>
2025-08-02 22:40:39 -0500

    update

--- app/Models/TipoAtencion.php
+++ app/Models/TipoAtencion.php
@@ -2,12 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TipoAtencion extends Model
 {
-    use HasFactory;
-
-    protected $table = "tipoAtencion";
+    //
 }
